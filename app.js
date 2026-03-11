0b4ab389269f06a0eb6cabb5411ab2564860d4f9
Otari1986 <otari198614@gmail.com>
2026-03-11 17:57:34 +0400

    b

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function card(item) {
     let isOut = item.stock <= 0
     let undiscounted = item.price.discountPercentage == 0
     return `<div class="card" onclick="showCardDetails('${item._id}')">
-                <img src="${item.thumbnail}" alt="">
+                <div class="cardPic"><img src="${item.thumbnail}" alt=""></div>
                 <h3>${item.title.slice(0, 18)}</h3>
                 <div class="price">
                     <p class="new">${item.price.currency} ${item.price.current}</p>
